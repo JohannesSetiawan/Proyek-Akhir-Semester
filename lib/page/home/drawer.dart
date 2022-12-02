0c7ebb6c071a2f07b26378e0d948186dd5d8ab9a
jaycent.gunawan <jaycent.gunawan@ui.ac.id>
2022-12-02 09:39:36 +0700

    fixing code based on flutter analyze

--- lib/page/home/drawer.dart
+++ lib/page/home/drawer.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_typing_uninitialized_variables, use_build_context_synchronously
+
 import 'package:nutrious/main.dart';
 import 'package:nutrious/page/home/profile.dart';
 import 'package:provider/provider.dart';
@@ -86,7 +88,7 @@ class DrawerMenu extends StatelessWidget {
             },
           ),
           ListTile(
-            trailing: Icon(Icons.exit_to_app, color: Colors.redAccent,),
+            trailing: const Icon(Icons.exit_to_app, color: Colors.redAccent,),
             title: const Text("Log Out", style: TextStyle(
                 fontWeight: FontWeight.w700,
                 color: Colors.redAccent
@@ -96,7 +98,7 @@ class DrawerMenu extends StatelessWidget {
               if (response != null){
                 Navigator.pushReplacement(
                     context,
-                    MaterialPageRoute(builder: (context) => MyApp())
+                    MaterialPageRoute(builder: (context) => const MyApp())
                 );
               }
             },
