c749ab43cdbc858123d02d9c99698bcd9d01ce06
annavaws <annavaws@gmail.com>
2022-12-08 09:01:56 +0700

    hapus warning

--- lib/page/home/user_dashboard.dart
+++ lib/page/home/user_dashboard.dart
@@ -308,6 +308,17 @@ class _UserDashboardState extends State<UserDashboard> {
                           ),
                           onTap: (){
                             // insert navigator
+                             Navigator.of(context).pushReplacementNamed(
+                                "/foodsharing_page",
+                                arguments: UserArguments(
+                                    args.isAdmin,
+                                    args.username,
+                                    args.nickname,
+                                    args.desc,
+                                    args.profURL,
+                                    args.isVerified
+                                )
+                            );
                           },
                         ),
                       ),
